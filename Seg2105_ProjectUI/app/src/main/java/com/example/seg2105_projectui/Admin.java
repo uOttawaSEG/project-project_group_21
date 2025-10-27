7f16973a32f1a8f0490beb2cd46e784d86699022
Jar073 <james@justdrive.ca>
2025-10-27 12:34:35 -0400

    small prep changes

--- Seg2105_ProjectUI/app/src/main/java/com/example/seg2105_projectui/Admin.java
+++ Seg2105_ProjectUI/app/src/main/java/com/example/seg2105_projectui/Admin.java
@@ -1,22 +1,42 @@
 package com.example.seg2105_projectui;
 
+
 import androidx.appcompat.app.AppCompatActivity;
+
+
+import java.lang.reflect.Member;
+
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.*;
 
+
 public class Admin extends User{
+
 
     public Admin(String userName, String userPassword){
         super(userName,userPassword);
     }
     //Admin will eventually have to log in
 
-    public approveMember(Member user){
+
+    public void approveMember(Member user){
         user.setAccountStatus(1);
     }
 
-    public rejectMember(Member user){
+
+    public void rejectMember(Member user){
         user.setAccountStatus(2);
+    }
+
+
+    public Member getNextRejected(){
+        return null;
+    }
+
+
+    public Member getNextPending(){
+        return null;
     }
 }
--- Seg2105_ProjectUI/app/src/main/java/com/example/seg2105_projectui/Member.java
+++ Seg2105_ProjectUI/app/src/main/java/com/example/seg2105_projectui/Member.java
@@ -1,21 +1,26 @@
 package com.example.seg2105_projectui;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.*;
+
 
 public class Member extends User{
     private String firstName;
     private String lastName;
     private String phoneNumber;
 
+
     private String userRole;
+
 
     //0 = pending
     //1 = approved
     //2 = rejected
     private int accountStatus;
+
 
     public Member(String userName, String userPassword, String userLastName, String userFirstName, String userPhoneNumber, String userRole){
         super(userName,userPassword);
@@ -26,21 +31,30 @@ public class Member extends User{
         this.accountStatus = 0;
     }
 
+
     public String getFirstName(){
         return firstName;
     }
+
 
     public String getLastName(){
         return lastName;
     }
 
+
     public String getPhoneNumber(){
         return phoneNumber;
     }
 
+
     public String getUserRole() { return userRole; }
-    
+   
     public int getAccountStatus() {
         return accountStatus;
+    }
+
+
+    public void setAccountStatus(int status){
+        this.accountStatus = status;
     }
 }
