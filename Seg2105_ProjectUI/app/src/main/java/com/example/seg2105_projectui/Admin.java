514024b582d9a111f44daaf5f8c33fb96bea36dc
Jar073 <james@justdrive.ca>
2025-10-27 22:25:34 -0400

    update admin

--- Seg2105_ProjectUI/app/src/main/java/com/example/seg2105_projectui/Admin.java
+++ Seg2105_ProjectUI/app/src/main/java/com/example/seg2105_projectui/Admin.java
@@ -1,10 +1,9 @@
 package com.example.seg2105_projectui;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-
-import java.lang.reflect.Member;
+import com.example.seg2105_projectui.Member;
 
 
 import android.os.Bundle;
@@ -13,31 +12,9 @@ import android.widget.*;
 
 
 public class Admin extends User{
-    private Member currentFile;
 
     public Admin(String userName, String userPassword){
         super(userName,userPassword);
-        this.currentFile = null;
-    }
-    //Admin will eventually have to log in
-
-
-    public void approveMember(Member user){
-        //user.setAccountStatus(1);
     }
 
-
-    public void rejectMember(Member user){
-        //user.setAccountStatus(2);
-    }
-
-
-    public void getNextRejected(){
-        this.currentFile = null;
-    }
-
-
-    public void getNextPending(){
-        this.currentFile = null;
-    }
 }
