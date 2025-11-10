5db29a2a53a3b469e6b99e294fa85e8c964331f6
marianne <eliza.marianne.aguhar@gmail.com>
2025-11-10 17:47:37 -0500

    added automatic approval ui

--- Seg2105_ProjectUI/app/src/main/java/com/example/seg2105_projectui/TutorActivity.java
+++ Seg2105_ProjectUI/app/src/main/java/com/example/seg2105_projectui/TutorActivity.java
@@ -2,12 +2,18 @@ package com.example.seg2105_projectui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class TutorActivity extends AppCompatActivity {
 
     private Button btnManage, btnPastSessions, btnUpcomingSessions, btnLogout, btnApproveSessions;
+
+    private Switch switchAutomaticApproval;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,7 @@ public class TutorActivity extends AppCompatActivity {
         btnUpcomingSessions = findViewById(R.id.btnUpcomingSessions);
         btnLogout = findViewById(R.id.btnLogout);
         btnApproveSessions = findViewById(R.id.btnApproveSessions);
+        switchAutomaticApproval = findViewById(R.id.toggleAutomaticApproval);
 
         String tutorUsername = getIntent().getStringExtra("username");
         System.out.println(tutorUsername);
@@ -58,6 +65,18 @@ public class TutorActivity extends AppCompatActivity {
             intent14.putExtra("username", tutorUsername);
             startActivity(intent14);
 
+        });
+
+        switchAutomaticApproval.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    btnApproveSessions.setVisibility(View.GONE); //make the manual approval section inaccessible
+                    //automatically approve all requests w/ the function
+                } else {
+                    btnApproveSessions.setVisibility(View.VISIBLE);
+                }
+            }
         });
     }
 }
